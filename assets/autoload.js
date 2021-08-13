e9f0bfc3076f122a6001927d043171103250f868
byt19990101 <bian9911@qq.com>
2021-08-13 13:47:38 +0800

    Update autoload.js

--- assets/autoload.js
+++ assets/autoload.js
@@ -1,8 +1,8 @@
 try {
-    $("<link>").attr({href: "https://cdn.jsdelivr.net/gh/byt19990101/live2d_2018/assets/waifu.min.css?v=1.4.2", rel: "stylesheet", type: "text/css"}).appendTo('head');
+    $("<link>").attr({href: "https://cdn.jsdelivr.net/gh/byt19990101/live2d_2018@release/assets/waifu.min.css?v=1.4.2", rel: "stylesheet", type: "text/css"}).appendTo('head');
     $('body').append('<div class="waifu"><div class="waifu-tips"></div><canvas id="live2d" class="live2d"></canvas><div class="waifu-tool"><span class="fui-home"></span> <span class="fui-chat"></span> <span class="fui-eye"></span> <span class="fui-user"></span> <span class="fui-photo"></span> <span class="fui-info-circle"></span> <span class="fui-cross"></span></div></div>');
-    $.ajax({url: 'https://cdn.jsdelivr.net/gh/byt19990101/live2d_2018/assets/waifu-tips.min.js?v=1.4.2',dataType:"script", cache: true, async: false});
-    $.ajax({url: 'https://cdn.jsdelivr.net/gh/byt19990101/live2d_2018/assets/live2d.min.js?v=1.0.5',dataType:"script", cache: true, async: false});
+    $.ajax({url: 'https://cdn.jsdelivr.net/gh/byt19990101/live2d_2018@release/assets/waifu-tips.min.js?v=1.4.2',dataType:"script", cache: true, async: false});
+    $.ajax({url: 'https://cdn.jsdelivr.net/gh/byt19990101/live2d_2018@release/assets/live2d.min.js?v=1.0.5',dataType:"script", cache: true, async: false});
     /* 可直接修改部分参数 */
     live2d_settings['hitokotoAPI'] = 'hitokoto.cn';  // 一言 API
     live2d_settings['modelId'] = 1;                  // 默认模型 ID
